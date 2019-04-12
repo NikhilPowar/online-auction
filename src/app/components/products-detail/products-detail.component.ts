a3194c7fb20af32ecab6429f78468eac08be9dd8
nikhilpowar <nikhilpowar16@gmail.com>
2019-04-12 19:32:28 +0530

    anonymized auction, created quick bids

--- src/app/components/products-detail/products-detail.component.ts
+++ src/app/components/products-detail/products-detail.component.ts
@@ -6,7 +6,6 @@ import { UserService } from '../../services/user.service';
 import { ProductsService } from '../../services/products.service';
 import { ProductModel, AuctionModel } from '../../models/product.model';
 
-
 @Component({
   selector: 'app-products-detail',
   templateUrl: './products-detail.component.html',
@@ -19,6 +18,7 @@ export class ProductsDetailComponent implements OnInit {
   productDetail: ProductModel;
   auctions: AngularFireList<AuctionModel>;
   auctionsDetail: AuctionModel[];
+  highestBid: AuctionModel;
   id: String;
   auctionNotCompleted = false;
   bidAmount: number = null;
@@ -47,6 +47,7 @@ export class ProductsDetailComponent implements OnInit {
         this.auctions.valueChanges().subscribe((obj: AuctionModel[]) => {
           this.auctionsDetail = obj;
           this.checkFn();
+          this.getHighestBid();
         });
       });
     });
@@ -84,31 +85,71 @@ export class ProductsDetailComponent implements OnInit {
     return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
   }
 
+  getHighestBid() {
+    const highestBidAmount = this.productDetail.BidStartingAmount;
+    if (this.auctionsDetail && this.auctionsDetail.length) {
+      this.highestBid = this.auctionsDetail[this.auctionsDetail.length - 1];
+    }
+  }
+
+  raiseBid(amount: number) {
+    amount = this.highestBid.Bid.valueOf() + amount;
+
+    const currentDate = new Date();
+    const bidEndDate = new Date(<any>this.productDetail.AutionEndTimeStamp);
+    if (bidEndDate <= currentDate) {
+      this.ErrorMessage = 'Bids closed';
+      this.checkFn();
+      return;
+    }
+
+    // tslint:disable-next-line:max-line-length
+    const c = confirm('Are you sure you want to bid ' + amount.toString() + ' on this product? This action will be final.');
+    if (!c) {
+      return;
+    }
+
+    // check again to verify no other higher bid has been made during time taken for confirmation
+    amount = Number(amount);
+    if (amount <= this.highestBid.Bid) {
+      this.ErrorMessage = 'Someone else bid a greater amount. Please try again.';
+      return;
+    }
+
+    const obj: AuctionModel = {
+      uid: this.user.uid,
+      pid: this.id,
+      FirstName: this.user.FirstName,
+      LastName: this.user.LastName,
+      Bid: amount,
+      TimeStamp: Date.now(),
+      DateTime: (new Date()).toString()
+    };
+    this.productsService.addAuction(obj);
+    this.bidAmount = null;
+    this.ErrorMessage = '';
+  }
+
   submitBid() {
     if (!this.bidAmount) {
-      this.ErrorMessage = 'Please Enter Bid Amount';
+      this.ErrorMessage = 'Please enter bid amount';
       return;
     }
     this.bidAmount = Number(this.bidAmount);
     if (isNaN(this.bidAmount)) {
-      this.ErrorMessage = 'Please Enter Bid Amount in Number';
+      this.ErrorMessage = 'Please enter bid amount as number';
       return;
     }
 
-    let lastBid = this.productDetail.BidStartingAmount;
-    if (this.auctionsDetail && this.auctionsDetail.length) {
-      lastBid = this.auctionsDetail[this.auctionsDetail.length - 1].Bid;
-    }
-
-    if (this.bidAmount <= lastBid) {
-      this.ErrorMessage = 'Bid amount should be greater than ' + lastBid;
+    if (this.bidAmount <= this.highestBid.Bid) {
+      this.ErrorMessage = 'Bid amount must be greater than ' + this.highestBid;
       return;
     }
 
     const currentDate = new Date();
     const bidEndDate = new Date(<any>this.productDetail.AutionEndTimeStamp);
     if (bidEndDate <= currentDate) {
-      this.ErrorMessage = 'Bid Closed';
+      this.ErrorMessage = 'Auction closed';
       this.checkFn();
       return;
     }
